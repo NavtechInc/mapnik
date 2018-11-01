c540b52c7569b662f6e6d81ecd5b137e3d12c61c
NavtechInc <AircraftPerform@navtech.aero>
2018-11-01 11:00:49 -0400

    Memory issues (revert changes)

--- include/mapnik/text/glyph_positions.hpp
+++ include/mapnik/text/glyph_positions.hpp
@@ -2,7 +2,7 @@
  *
  * This file is part of Mapnik (c++ mapping toolkit)
  *
- * Copyright (C) 2014 Artem Pavlenko
+ * Copyright (C) 2015 Artem Pavlenko
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,16 +19,21 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  *****************************************************************************/
-#ifndef MAPNIK_PLACEMENTS_LIST_HPP
-#define MAPNIK_PLACEMENTS_LIST_HPP
+
+#ifndef MAPNIK_TEXT_GLYPH_POSITIONS_HPP
+#define MAPNIK_TEXT_GLYPH_POSITIONS_HPP
+
 //mapnik
 #include <mapnik/box2d.hpp>
 #include <mapnik/pixel_position.hpp>
 #include <mapnik/text/rotation.hpp>
 #include <mapnik/marker_cache.hpp>
+#include <mapnik/text/glyph_info.hpp>
 
-// agg
+#pragma GCC diagnostic push
+#include <mapnik/warning_ignore_agg.hpp>
 #include "agg_trans_affine.h"
+#pragma GCC diagnostic pop
 
 //stl
 #include <vector>
@@ -36,8 +41,6 @@
 
 namespace mapnik
 {
-
-struct glyph_info;
 
 struct glyph_position
 {
@@ -50,18 +53,20 @@ struct glyph_position
 
 struct marker_info
 {
-    marker_info() : marker(), transform() {}
-    marker_info(marker_ptr _marker, agg::trans_affine const& _transform) :
-        marker(_marker), transform(_transform) {}
-    marker_ptr marker;
-    agg::trans_affine transform;
+    //marker_info() : marker(), transform() {}
+    marker_info(std::shared_ptr<marker const> _marker, agg::trans_affine const& _transform) :
+        marker_(_marker), transform_(_transform) {}
+    std::shared_ptr<marker const> marker_;
+    agg::trans_affine transform_;
 };
-using marker_info_ptr = std::shared_ptr<marker_info>;
 
+using marker_info_ptr = std::shared_ptr<marker_info>;
+        
 /** Stores positions of glphys.
  *
  * The actual glyphs and their format are stored in text_layout.
  */
+
 class glyph_positions
 {
 public:
@@ -71,7 +76,12 @@ public:
     std::size_t size() const { return data_.size(); };
     const_iterator begin() const;
     const_iterator end() const;
-
+    
+    /*
+     using iterator = std::vector<glyph_position>::iterator;
+    iterator begin() { return data_.begin(); }
+    iterator end() { return data_.end(); }
+     */
     void emplace_back(glyph_info const& glyph, pixel_position offset, rotation const& rot);
     void reserve(unsigned count);
 
@@ -79,25 +89,65 @@ public:
     void set_base_point(pixel_position const& base_point);
     //WI: Added John's changes for marker rotation 13NOV2014 (identity transform in general)
     void set_marker(marker_info_ptr marker, pixel_position const& marker_pos, box2d<double> const& marker_bbox,  agg::trans_affine const& marker_placement_tr= agg::trans_affine());
+    pixel_position const& marker_pos() const;
     
-    marker_info_ptr marker() const;
-    pixel_position const& marker_pos() const; 
+    marker_info_ptr const& get_marker() const;
     box2d<double> const & bbox() const;
     box2d<double> const & marker_bbox() const;
     agg::trans_affine const& marker_placement_tr() const { return marker_placement_tr_; }
+
+    void clear_glyps() { data_.clear(); }
     
 private:
     std::vector<glyph_position> data_;
     pixel_position base_point_;
-    marker_info_ptr marker_;
+    marker_info_ptr marker_info_;
     pixel_position marker_pos_;
     //WI: Added John's changes for marker rotation 13NOV2014
     agg::trans_affine marker_placement_tr_;
     box2d<double> bbox_;
     box2d<double> marker_bbox_;
 };
-using glyph_positions_ptr = std::shared_ptr<glyph_positions>;
+using glyph_positions_ptr = std::unique_ptr<glyph_positions>;
+
 
 using placements_list = std::list<glyph_positions_ptr>;
-}
-#endif // PLACEMENTS_LIST_HPP
+
+struct scoped_glyph_positions_offset
+{
+    scoped_glyph_positions_offset(glyph_positions & glyphs, pixel_position const& offset)
+        : glyphs_(glyphs)
+        , base_point_(glyphs.get_base_point())
+        , marker_pos_(glyphs.marker_pos())
+    {
+        // move the glyphs to the correct offset
+        glyphs_.set_base_point(base_point_ + offset);
+
+        // update the position of any marker
+        if (auto const& marker_info = glyphs_.get_marker())
+        {
+           glyphs_.set_marker(marker_info, marker_pos_ + offset, glyphs_.marker_bbox());
+        }
+    }
+
+    ~scoped_glyph_positions_offset()
+    {
+        // set the base_point back how it was
+        glyphs_.set_base_point(base_point_);
+
+        // restore marker as well, if there is any
+        if (auto const& marker_info = glyphs_.get_marker())
+        {
+            glyphs_.set_marker(marker_info, marker_pos_, glyphs_.marker_bbox());
+        }
+    }
+
+private:
+    glyph_positions & glyphs_;
+    pixel_position base_point_;
+    pixel_position marker_pos_;
+};
+
+} // namespace mapnik
+
+#endif // MAPNIK_TEXT_GLYPH_POSITIONS_HPP
